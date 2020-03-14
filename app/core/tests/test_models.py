1c03f9ea9a6df990c17f7dc04c56cea88a90e5f0
DuyNguyen <llduyll10@gmail.com>
2020-03-14 16:37:17 +0700

    a

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -50,3 +50,11 @@ class ModelTests(TestCase):
             name='Duymap'
         )
         self.assertEqual(str(tag),tag.name)
+
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='hahaduymap'
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
