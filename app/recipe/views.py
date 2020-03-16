4743206ee35ca45105b8401778a0b64fff9d6ea4
DuyNguyen <llduyll10@gmail.com>
2020-03-16 11:14:45 +0700

    Revert "demo heroku"
    This reverts commit 90f2ae6d8d1651b0d4c015433b0adee434ce3d1e.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -17,17 +17,8 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        """Return objects for current user"""
-        assigned_only = bool(
-            int(self.request.query_params.get('assigned_only', 0))
-        )
-        queryset = self.queryset
-        if assigned_only:
-            queryset = queryset.filter(recipe__isnull=False)
-
-        return queryset.filter(
-            user=self.request.user
-        ).order_by('-name').distinct()
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
         """Create a new ingredient"""
@@ -51,11 +42,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-
     def _params_to_ints(self, qs):
         """Convert a list of string IDs to a list of integers"""
         return [int(str_id) for str_id in qs.split(',')]
-
+        
     def get_queryset(self):
         """Retrieve the recipes for the authenticated user"""
         tags = self.request.query_params.get('tags')
